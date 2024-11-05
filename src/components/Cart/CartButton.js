3858a1cd149f73fad6d6b74f5086e2fea39eafd5
ragul2808 <ragulraja28@gmail.com>
2024-11-05 09:14:26 +0530

    implement the toggle function in uislice

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,8 +1,15 @@
+import { useDispatch } from 'react-redux';
+import { uiActions } from '../../store/ui-slice';
 import classes from './CartButton.module.css';
 
 const CartButton = (props) => {
+  const dispatch = useDispatch();
+
+  const toogleCartHandler = () => {
+    dispatch(uiActions.toggle())
+  }
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={toogleCartHandler}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
